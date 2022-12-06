7b0f04379810a1bcf2c7f7837013902f0f50df61
zhuchichi56 <75229525+zhuchichi56@users.noreply.github.com>
2022-12-06 08:10:59 +0800

    Initial commit

---
+++ src/dependency_injection/BeanFactoryImpl.java
@@ -0,0 +1,22 @@
+package dependency_injection;
+
+import java.io.File;
+
+/**
+ * TODO you should complete the class
+ */
+public class BeanFactoryImpl implements BeanFactory {
+
+    @Override
+    public void loadInjectProperties(File file) {
+    }
+
+    @Override
+    public void loadValueProperties(File file) {
+    }
+
+    @Override
+    public <T> T createInstance(Class<T> clazz) {
+        return null;
+    }
+}
